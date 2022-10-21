593d057e11fd36b2f1bb6f1b12d45b3f28d0e789
adicerbo <dicerboantonio32@gmail.com>
2022-10-21 13:16:45 -0400

    working routes and base partials

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     
 ]
