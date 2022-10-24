88e6cc9d126d702463bd03d6e024af8e574ce6d6
adicerbo <dicerboantonio32@gmail.com>
2022-10-24 14:22:15 -0400

    working details page for each boat

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('boats/', views.boats_index, name='index'),
-    # path('boats/<int:boat_id>/', views.boats_detail, name='detail'),
+    path('boats/<int:boat_id>/', views.boats_detail, name='detail'),
     # path('boats/create', views.BoatCreate.as_view(), name='boat_create'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,7 @@ def about(request):
 def boats_index(request):
     boats = Boat.objects.all()
     return render(request, 'boats/index.html', { 'boats': boats })
+
+def boats_detail(request, boat_id):
+    boat = Boat.objects.get(id=boat_id)
+    return render(request, 'boats/detail.html', {'boat': boat})
